1ff664144a7edf25c596d7e4c473021173b8c6f5
Mrinal <mrinalmallik1@gmail.com>
2023-03-23 14:25:08 +0600

    remove unnecessary

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,38 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <div class="app flex">
+      <Navigation />
+      <router-view />
+    </div>
+  </div>
 </template>
 
+<script>
+import Navigation from "./components/Navigation.vue";
+export default {
+  name: "App",
+  components: { Navigation },
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
 }
-
+.app {
+  background-color: #141625;
+  min-height: 100vh;
+  @media (min-width: 900px) {
+    flex-direction: row !important;
+  }
+}
+.app-content {
+  padding: 0 20px;
+  flex: 1;
+  position: relative;
+}
 nav {
   padding: 30px;
 }
