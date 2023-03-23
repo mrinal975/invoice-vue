a3fc36c158155b330598cdfabe7561b28332b173
Mrinal <mrinalmallik1@gmail.com>
2023-03-23 17:31:08 +0600

    sass integraded

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/Home.vue";
 const routes = [
-  // {
-  //   path: "/",
-  //   name: "home",
-  //   component: HomeView,
-  // },
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
   // {
   //   path: "/about",
   //   name: "about",
