ae8570b9df39f74a3f970eeff05d975a81c2794e
Mrinal <mrinalmallik1@gmail.com>
2023-03-23 20:37:58 +0600

    invoice data added

---
+++ src/components/InvoiceModal.vue
@@ -0,0 +1,31 @@
+<template></template>
+<script>
+export default {
+  name: "invoiceModal",
+  data() {
+    return {
+      billerStreetAddress: null,
+      billerCity: null,
+      billerZipCode: null,
+      billerCountry: null,
+      clientName: null,
+      clientEmail: null,
+      clientStreetAddress: null,
+      clientCity: null,
+      clientZipCode: null,
+      clientCountry: null,
+      invoiceDateUnix: null,
+      invoiceDate: null,
+      paymentTerms: null,
+      paymentDueDateUnix: null,
+      paymentDueDate: null,
+      productDescription: null,
+      invoicePending: null,
+      invoiceDraft: null,
+      invoiceItemList: [],
+      invoiceTotal: 0,
+    };
+  },
+};
+</script>
+<style></style>
