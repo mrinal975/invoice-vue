02a06519274069ca0ec58383d59db7b949eb806a
Mrinal <mrinalmallik1@gmail.com>
2023-03-24 17:57:17 +0600

    invoiceview

--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -1,6 +1,32 @@
-<template></template>
+<template>
+  <div>
+    {{ currentInvoice }}
+    - hi
+  </div>
+</template>
 <script>
+import { mapMutations, mapState } from "vuex";
 export default {
   name: "InvoiceView",
+  date() {
+    return {
+      currentInvoice: null,
+    };
+  },
+  created() {
+    this.getCurrentInvocie();
+    this.currentInvoice = this.currentInvoiceArray[0];
+    console.log("joy baba loknath");
+  },
+  methods: {
+    ...mapMutations(["SET_CURRENT_INVOICE"]),
+    getCurrentInvocie() {
+      this.SET_CURRENT_INVOICE(this.$route.params.invoiceId);
+      console.log(this.$route.params.invoiceId);
+    },
+  },
+  computed: {
+    ...mapState(["currentInvoiceArray"]),
+  },
 };
 </script>
