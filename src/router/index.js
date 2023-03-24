8ecbcde2883f7054b283beb8f24cc61fb995422f
Mrinal <mrinalmallik1@gmail.com>
2023-03-24 17:00:01 +0600

    invoice view added

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/Home.vue";
+import InvoiceView from "../views/InvoiceView.vue";
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
   },
-  // {
-  //   path: "/about",
-  //   name: "about",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: function () {
-  //     return import(/* webpackChunkName: "about" */ "../views/AboutView.vue");
-  //   },
-  // },
+  {
+    path: "/invoice/:invoiceId",
+    name: "Invoice",
+    component: InvoiceView,
+  },
 ];
 
 const router = createRouter({
